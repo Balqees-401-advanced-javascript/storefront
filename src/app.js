698dad94a576a4ded757785dd459b3f56a02c05a
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-07-13 08:40:55 +0300

    prep

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Footer from './components/footer';
+import Header from './components/header';
+
+import './style.scss';
+
+export default props => {
+    return (
+        <>
+         <Header />
+         <Footer />
+        </>
+    )
+};
