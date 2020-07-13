0b28912163ce8a883506294d0522c5727a454b99
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-07-14 01:16:57 +0300

    done

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './componants/header';
+import Footer from './componants/footer';
+import Products from './componants/products';
+import Categories from './componants/categories';
+import ActiveCategory from './componants/activecategory';
+import './style.scss';
+export default props => {
+    return (
+        <>
+    
+            <Header />
+            <Categories />
+            <ActiveCategory />
+            <Products />
+            <Footer />
+        </>
+    )
+}
